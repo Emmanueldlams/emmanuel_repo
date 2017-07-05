ed6537efc2b45928b3d7c1ac356aa0f0313eb2ad
Mduduzi Dlamini <mdudlamini2003@yahoo.com>
2017-07-05 14:34:45 +0200

    Add files via upload

--- mailcount.php
+++ mailcount.php
@@ -1,11 +1,15 @@
 <?php
 
-$mbox = imap_open ("{imap.gmail.com:993/imap/ssl/novalidate-cert/norsh}Inbox", "example@gmail.com", "***********", OP_READONLY) 
+$mbox = imap_open ("{imap.gmail.com:993/imap/ssl/novalidate-cert/norsh}Inbox", "mrango2011@gmail.com", "wordpass15@M", OP_READONLY) 
 or die("can't connect: " . imap_last_error()); 
 $check = imap_mailboxmsginfo($mbox); 
 $number_of_messages = $check->{'Nmsgs'};
 if ($number_of_messages<=1000) { 
-print "Total number of messages is: ". $check->Unread; //. "/" . $check->Nmsgs; 
+//print var_dump ($check);
+echo "<br \><br \>\n";
+print "Number of unread messages is: ". $check->Unread; //. "/" . $check->Nmsgs; 
+echo "<br /><br \>\n";
+print "The total number of all e-mail messages is: ".$check->Nmsgs; 
 } else { 
 print "Failed, there are too many messages"; 
 }
